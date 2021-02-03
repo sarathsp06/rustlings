07ff7e8d17877ad264bc414699ab0c80cb41bffc
sarathsp06 <sarath.sp06@gmail.com>
2021-02-03 12:02:05 +0530

    another day

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,18 +1,20 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
+    };
+    ($a:String, $b:bool) => {
+        println!("Look at this other macro: {},{}", $a,$b);
     }
 }
 
 fn main() {
     my_macro!();
     my_macro!(7777);
+    my_macro!("dd",true);
 }
