e9b0861dcef24dcfdb7e7bb0a3611ef405a8e6bd
sarathsp06 <sarath.sp06@gmail.com>
2021-03-03 05:04:40 +0530

    one more day

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,14 +8,16 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
-
+    fn append_bar(self) -> String {
+        let mut  f = self.to_owned();
+        f.push_str("Bar");
+        f
+    }   
 }
 
 fn main() {
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,14 +10,17 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
-
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut f  = self.to_owned();
+        f.push("Bar".to_owned());
+        f
+    }   
+}
 
 
 
